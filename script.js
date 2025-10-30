98a0a8546b013d2a715784f326bc8252a313d3d2
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2025-10-30 22:02:40 +0100

    ajouter section1

---
+++ script.js
@@ -0,0 +1,36 @@
+function dispalysection1() {
+  const section1 = document.getElementById("section1");
+  const aside = document.getElementById("aside");
+  const section2 = document.getElementById("section2");
+  aside.style.display = "none";
+  section1.style.display = "none";
+  section2.style.display = "block";
+}
+const btns = document.querySelectorAll(".botton");
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const eventcard = e.currentTarget.closest(".card");
+    let objetEvent = {
+      image: eventcard.querySelector("img").src,
+      title: eventcard.querySelector("h3").textContent,
+      details: Array.from(eventcard.querySelectorAll("ul li")).map(
+        (li) => li.textContent
+      ),
+      price: eventcard.querySelector("p").textContent,
+    };
+    console.log(objetEvent);
+    const div2 = document.querySelector("#div");
+    div2.innerHTML = `<img src="${objetEvent.image}"/>
+          <h3>${objetEvent.title}</h3>
+          <ul>
+            <li>${objetEvent.details[0]}</li>
+            <li>${objetEvent.details[1]}</li>
+            <li>${objetEvent.details[2]}</li>
+          </ul>
+          <button
+            onclick="dispalysection1()"
+            type="button"
+          ></button>
+          <p></p>`;
+  });
+});
